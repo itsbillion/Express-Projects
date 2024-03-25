219bdf448a0456803b81c6a3cb641c23ff1028e3
itsbillion <pc.kaydee11@gmail.com>
2024-03-25 09:20:31 +0100

    Update new_index.js

--- Simple server/new_index.js
+++ Simple server/new_index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
 app.get("/", (req, res) => {
-  res.send(<h1>Welcome!!!</h1>)
+  res.send("<h1>Welcome!!!</h1>")
 });
 app.get("/about", (req, res) => {
-  res.send("ABOUT US")
+  res.send("<h1>ABOUT US</h1>")
 });
 
 let PORT = process.env.PORT || 5000;
